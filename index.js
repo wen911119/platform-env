83b3739a8defeba87067cb4cec8bfc65c7d4efbd
wen911119 <wen911119@gmail.com>
2020-02-28 16:30:04 +0800

    feat:增加loading

--- index.js
+++ index.js
@@ -6,13 +6,20 @@ const load  = (src, success, fail) => {
   document.body.appendChild(script)
 }
 
+import indicator from 'h5-indicator'
+
 export const getWechatWx = () => {
   if (window.wx) {
     return window.wx
   }
   return new Promise((resolve, reject)=>{
+    const lid = indicator.showLoading()
     load('https://res.wx.qq.com/open/js/jweixin-1.6.0.js', () => {
+      indicator.hideLoading(lid)
       resolve(window.wx)
-    }, reject)
+    }, () => {
+      indicator.hideLoading(lid)
+      reject('wx script loading fail')
+    })
   })
 }
