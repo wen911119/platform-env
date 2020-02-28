f4856c93fc92f4c9a89854e437336238d0bef9e8
wen911119 <wen911119@gmail.com>
2020-02-28 17:02:33 +0800

    fix#promise 错误

--- index.js
+++ index.js
@@ -1,25 +1,30 @@
-const load  = (src, success, fail) => {
-  const script = document.createElement('script')
-  script.src = src
-  script.onload = success
-  script.onerror = fail
-  document.body.appendChild(script)
-}
+const load = (src, success, fail) => {
+  const script = document.createElement("script");
+  script.src = src;
+  script.onload = success;
+  script.onerror = fail;
+  document.body.appendChild(script);
+};
 
-import indicator from 'h5-indicator'
+import indicator from "h5-indicator";
 
 export const getWechatWx = () => {
-  if (window.wx) {
-    return window.wx
-  }
-  return new Promise((resolve, reject)=>{
-    const lid = indicator.showLoading()
-    load('https://res.wx.qq.com/open/js/jweixin-1.6.0.js', () => {
-      indicator.hideLoading(lid)
-      resolve(window.wx)
-    }, () => {
-      indicator.hideLoading(lid)
-      reject('wx script loading fail')
-    })
-  })
-}
+  return new Promise((resolve, reject) => {
+    if (window.wx) {
+      resolve(window.wx);
+    } else {
+      const lid = indicator.showLoading();
+      load(
+        "https://res.wx.qq.com/open/js/jweixin-1.6.0.js",
+        () => {
+          indicator.hideLoading(lid);
+          resolve(window.wx);
+        },
+        () => {
+          indicator.hideLoading(lid);
+          reject("wx script loading fail");
+        }
+      );
+    }
+  });
+};
